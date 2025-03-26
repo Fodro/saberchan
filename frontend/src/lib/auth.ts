0a0053ba1fba5bd6f8c021685c9fbeed83c4e5d4
Фёдор Томилов <ftomilov@itmo.ru>
2025-03-26 22:15:22 +0300

    FRONT-002: fix logout and env

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,5 +1,5 @@
-import { OIDC_REALM, OIDC_CLIENT_ID, OIDC_CLIENT_SECRET, OIDC_REDIRECT_URI } from "$env/static/private";
+import { OIDC_REALM, OIDC_CLIENT_ID, OIDC_CLIENT_SECRET, AUTH_HOST } from "$env/static/private";
 import * as arctic from "arctic";
 
 export const codeVerifier = arctic.generateCodeVerifier();
-export const keycloak = new arctic.KeyCloak(OIDC_REALM, OIDC_CLIENT_ID, OIDC_CLIENT_SECRET, OIDC_REDIRECT_URI);
+export const keycloak = new arctic.KeyCloak(OIDC_REALM, OIDC_CLIENT_ID, OIDC_CLIENT_SECRET, `${AUTH_HOST}/admin/auth/signIn`);
