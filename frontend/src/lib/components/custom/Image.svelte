2a39f67a61c88cc0df8cf7a2cd70d2a2cfad2cd1
Фёдор Томилов <ftomilov@itmo.ru>
2025-08-02 00:16:27 +0300

    fix minors

--- frontend/src/lib/components/custom/Image.svelte
+++ frontend/src/lib/components/custom/Image.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Button from "../ui/button/button.svelte";
-	import * as Card from "$lib/components/ui/card/index.js";
 
 	let {
 		link,
@@ -9,24 +8,17 @@
 		link: string;
 		name: string;
 	} = $props();
-
-	let viewFull: boolean = $state(false);
-	
 </script>
 
 <div class="flex flex-col justify-center items-center">
 	<Button
 		variant="link"
 		class="cursor-pointer"
-		on:click={() => {
-			viewFull = !viewFull;
-		}}
+		href={`/view?link=${link}&name=${name}`}
+		target="_blank"
+		rel="noreferrer noopener"
 	>
 		{name}
 	</Button>
-	{#if viewFull}
-		<img src={link} alt={name} class="h-100 w-120 object-scale-down" />
-	{:else}
-		<img src={link} alt={name} class="h-48 w-72 object-scale-down" />
-	{/if}
+	<img src={link} alt={name} class="h-48 w-72 object-scale-down" />
 </div>
